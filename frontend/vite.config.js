31f0afc644e5cc828cbfe851d776593b88472096
anilkumarpaital1-art <anilkumarpaital1@gmail.com>
2026-07-04 22:42:41 +0530

    Optimize Vite build chunks

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    chunkSizeWarningLimit: 700,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          router: ["react-router-dom"],
+          axios: ["axios"],
+          icons: ["react-icons"],
+        },
+      },
+    },
+  },
+});
